f2455d0d0051d4b12622bb0a5f8a2a843a0984cf
buerkelHSE <buerkel@ph-heidelberg.de>
2024-11-15 12:40:16 +0100

    Flugzeug Klasse
    Flugzeug Gewicht
    Träger Kapazität

--- bsp_Flugzeugtraeger.py
+++ bsp_Flugzeugtraeger.py
@@ -3,18 +3,19 @@ Klasse für Flugzeugträger. Hat Liste mit Flugzeugen, maximale Anzahl, sowie Fu
 zum Hinzufügen und starten von Flugzeugen
 '''
 class Flugzeugtraeger:
-    def __init__(self, name, max_flugzeuge):
+    def __init__(self, name, kapazitaet, max_flugzeuge):
         self.name = name
         self.max_flugzeuge = max_flugzeuge
         self.flugzeuge = []
+        self.kapazitaet=kapazitaet
 
     #Entfernt Flugzeug von Liste und startet es
     def starte_flugzeug(self, flugzeug):
         if flugzeug in self.flugzeuge:
-            print("Flugzeug", flugzeug ,"startet...")
+            print("Flugzeug", flugzeug.name ,"startet...")
             # Simuliere Startvorgang
             self.flugzeuge.remove(flugzeug)
-            print("Flugzeug", flugzeug ,"gestartet.")
+            print("Flugzeug", flugzeug.name ,"gestartet.")
         else:
             print("Flugzeug nicht gefunden.")
         
@@ -26,8 +27,14 @@ class Flugzeugtraeger:
         else:
             print("Maximale Anzahl an Flugzeugen erreicht.")
 
+class Flugzeug:
+    def __init__(self, name, gewicht):
+        self.name = name
+        self.gewicht = gewicht
+        
 # Beispielinstanz erzeugen
-traeger = Flugzeugtraeger("Aircraft Carrier", 50)
+traeger = Flugzeugtraeger("Aircraft Carrier", 100000, 50)
 
-traeger.add_flugzeug("F-16")
-traeger.starte_flugzeug("F-16")  # Starten eines Flugzeugs
+flugz=Flugzeug("F-16",1000)
+traeger.add_flugzeug(flugz)
+traeger.starte_flugzeug(flugz)  # Starten eines Flugzeugs
